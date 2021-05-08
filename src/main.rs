00b93cc8de7eb8b1c9e2981b01e1d1d358542f7a
bow <pro.lpascal@gmail.com>
2021-05-08 12:04:38 +0200

    [add] Prime generation in its own module

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,38 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
-use primes::{Sieve, PrimeSet};
-use rocket::response::content::{Json};
-use std::time::{Instant};
+use rocket::response::content::Json;
 
-fn bake_prime(min: Option<u64>, _max : Option<u64>) -> (f64, u64) {
-    let start = Instant::now();
-    let mut pset = Sieve::new();
-    let min = match min {
-        Some(x) => x,
-        None => 0
-    };
-    let (_, prime) = pset.find(min);
-    let duration = start.elapsed();
-    (duration.as_secs() as f64 + duration.subsec_nanos() as f64 * 1e-9, prime)
-}
+mod prime;
 
-fn to_json(duration: f64, prime: u64) -> Json<String> {
-    Json(format!("{{ 'prime': {}, 'duration': {} }}", prime.to_string(), duration.to_string()))
+fn to_json(prime: prime::Prime) -> Json<String> {
+    Json(format!(
+        "{{ 'prime': {}, 'duration': {}, 'new': {} }}",
+        prime.value.to_string(),
+        prime.duration.to_string(),
+        prime.new.to_string()
+    ))
 }
 
 #[get("/")]
-fn root()  -> Json<String> {
-    let (duration, prime) = bake_prime(None, None);
-    to_json(duration, prime)
+fn root() -> Json<String> {
+    to_json(prime::random())
 }
 
-#[get("/range/<min>/<max>")]
-fn range(min: u64, max: u64) -> Json<String> {
-    let (duration, prime) = bake_prime(Some(min), Some(max));
-    to_json(duration, prime)
+#[get("/greater_than/<min>")]
+fn from(min: u64) -> Json<String> {
+    to_json(prime::greater_than(min))
+}
+
+#[get("/at_position/<idx>")]
+fn position(idx: u64) -> Json<String> {
+    to_json(prime::at_position(idx))
 }
 
 fn main() {
     let rocket = rocket::ignite();
-    let rocket = rocket.mount("/", routes![root, range]);
+    let rocket = rocket.mount("/", routes![root, from, position]);
     rocket.launch();
 }
